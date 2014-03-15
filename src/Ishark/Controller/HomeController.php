b2955b3ecc5fc229aae820f11b59ebacd7cb6763
Patrick Romowicz <p.romowicz@shopmacher.de>
2014-03-15 18:23:08 +0100

    header for caching

--- src/Ishark/Controller/HomeController.php
+++ src/Ishark/Controller/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends BaseController
      * @param Request $request
      * @param $image
      * @return Response
+     * @throws \Exception
      */
     public function imageAction(Request $request, $image)
     {
@@ -32,6 +33,32 @@ class HomeController extends BaseController
         $md5 = ConvertService::unpackmd5($info['filename']);
         $path = $this->getApp()->getRootPath() . '/images/' . substr($md5, 0, 2) . '/' . $md5 . '.' . $info['extension'];
 
+        if (!file_exists($path)) {
+            throw new \Exception('File not found!', 404);
+        }
+
+        $mtime = @filemtime($path);
+        $gmt_mtime = gmdate('D, d M Y H:i:s', $mtime) . ' GMT';
+        $etag = sprintf('%08x-%08x', crc32($path), $mtime);
+
+        header('ETag: "' . $etag . '"');
+        header('Last-Modified: ' . $gmt_mtime);
+        header('Cache-Control: private');
+
+        if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && !empty($_SERVER['HTTP_IF_NONE_MATCH'])) {
+            $tmp = explode(';', $_SERVER['HTTP_IF_NONE_MATCH']); // IE fix!
+            if (!empty($tmp[0]) && strtotime($tmp[0]) == strtotime($gmt_mtime)) {
+                header('HTTP/1.1 304 Not Modified');
+                exit;
+            }
+        }
+
+        if (isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+            if (str_replace(array('\"', '"'), '', $_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
+                header('HTTP/1.1 304 Not Modified');
+                exit;
+            }
+        }
         return Response::create(file_get_contents($path), 200, array('Content-Type' => ContentTypeService::toType($info['extension'])));
     }
 
