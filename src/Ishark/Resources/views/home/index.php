48d2b12eb97a02321696c68abbd0a34f75fa38c8
Patrick Romowicz <p.romowicz@shopmacher.de>
2014-03-15 16:32:12 +0100

    qr remove

--- src/Ishark/Resources/views/home/index.php
+++ src/Ishark/Resources/views/home/index.php
@@ -12,7 +12,6 @@
         <div id="ubh2">
             <input type="radio" id="r1" name="ult" value="f" checked="checked" accesskey="d" onchange="s(this)" /><label for="r1"> <u>D</u>atei</label>
             <input type="radio" id="r2" name="ult" value="u" accesskey="w" onchange="s(this)"/><label for="r2"> <u>W</u>ebadresse</label>
-            <input type="radio" id="r3" name="ult" value="q" accesskey="q" onchange="s(this)"/><label for="r3"> <u>Q</u>R-Code aus Text/URL</label>
         </div>
     </div></form>
 <form method="post" id="frm2">
